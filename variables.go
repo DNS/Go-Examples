3288aa227e548c1d51e08506cf29007ae019f96e
DNS <37093+DNS@users.noreply.github.com>
2025-02-27 17:12:13 +0700

    function

--- variables.go
+++ variables.go
@@ -3,8 +3,27 @@
 import "fmt"
 
 
+var a string
+var b = "Hello, world!"
+var c string = "Hello, world!"
+var d, e int
+var f, g int = 1, 2
+var h, i = 1, "Hello, world!"
+var j float64 = 0.12345
+const k int64 = 123456789
+const l, m = 1, 2
+const (
+	Z1 = 3.14
+	Z2 = "abc"
+	Z3
+)
+
 func main () {
-	var msg = "Hello, world!\n"
-	fmt.Printf(msg)
+	var msg = "Hello, world!"
+	fmt.Println(msg)
+	
+	msg1 := "Hello"
+	msg2 := 123
+	fmt.Printf("%s %d\n", msg1, msg2)
 }
 
