1343b90eebe038aad3ed813f38d7f87494940c33
DNS <37093+DNS@users.noreply.github.com>
2025-02-27 16:38:24 +0700

    stdout

--- stdout.go
+++ stdout.go
@@ -7,6 +7,6 @@ func main () {
 	fmt.Print("Hello, ")
 	fmt.Println("World!")
 	fmt.Printf("The number is: %d %f\n", 42)
-	fmt.Printf("Floating point is: %.30f\n", 0.12345678901234567890)
+	fmt.Printf("Floating point: %.30f\n", 0.12345678901234567890)
 }
 
