3e07cd3182581105e62df6050a5e11bedf4cb9db
DNS <37093+DNS@users.noreply.github.com>
2025-02-27 21:08:06 +0700

    new data types

--- numbers.go
+++ numbers.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	num1 := 3		  	// int
-	num2 := 3.		 	// float64
-	num3 := 3 + 4i	 	// complex128
-	num4 := byte('A')	// byte (alias for uint8)
+	num1 := 3		  		// int
+	num2 := 3.		 		// float64
+	num3 := 3 + 4i	 		// complex128
+	num4 := byte('A')		// byte (alias for uint8)
+	var num5 uint = 7			// uint (unsigned)
+	var num6 float32 = 22.7	// 32-bit float
 	
 	fmt.Println(num1)
 	fmt.Println(num2)
 	fmt.Println(num3)
 	fmt.Println(num4)
+	fmt.Println(num5)
+	fmt.Println(num6)
 }
 
 
