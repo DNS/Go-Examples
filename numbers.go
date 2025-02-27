56f96f28df2e5a44df638ff94561a0d1186b0c2c
DNS <37093+DNS@users.noreply.github.com>
2025-02-27 17:21:02 +0700

    numbers

---
+++ numbers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 3		  	// int
+	num2 := 3.		 	// float64
+	num3 := 3 + 4i	 	// complex128
+	num4 := byte('A')	// byte (alias for uint8)
+	
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num3)
+	fmt.Println(num4)
+}
+
+
+
+
